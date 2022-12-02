238112e534ed1005e465b64ecb467effb41347fd
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-02 18:45:58 +0800

    chore: refactor

--- server/Configs.py
+++ server/Configs.py
@@ -1,5 +1,7 @@
-IP_ADDRESS = "127.0.0.1"
+# To connect anywhere in the private network using all available private interfaces, set IP_ADDRESS to "0.0.0.0"
+IP_ADDRESS = "0.0.0.0"                  
+
 PORT_NUMBER = 3000
 
 
-ALLOW_BLOCKING_PROTOCOL = True
+ALLOW_BLOCKING_PROTOCOL = True          # Disable this if blocking/unblocking is to be removed.
