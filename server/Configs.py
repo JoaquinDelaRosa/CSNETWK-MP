15130e8ae17b8fe33e2ba1e990f02a83032ac702
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-05 15:03:48 +0800

    update: configs for demo kit.

--- server/Configs.py
+++ server/Configs.py
@@ -1,7 +1,7 @@
 # To connect anywhere in the private network using all available private interfaces, set IP_ADDRESS to "0.0.0.0"
-IP_ADDRESS = "0.0.0.0"                  
+IP_ADDRESS = "127.0.0.1"                  
 
-PORT_NUMBER = 3000
+PORT_NUMBER = 12345
 
 
 ALLOW_BLOCKING_PROTOCOL = True          # Disable this if blocking/unblocking is to be removed.
